0e9e50d2a2fd99f188f37eb916469636d1bbc81d
Fred Jodozi <fredfred776@gmail.com>
2023-10-24 17:00:35 -0400

    add downSrolling functionality

--- index.js
+++ index.js
@@ -1,15 +1,38 @@
-const sections = document.querySelectorAll('home, section, footer');
-const scrollButton = document.getElementById('scroll-button');
+const sections = document.querySelectorAll('section');
+const downScrollButton = document.querySelector('#scroll-button');
 let currentSectionIndex = 0;
 
-scrollButton.addEventListener("click", () => {
+const handleScrollDown = () => {
   if (currentSectionIndex < sections.length -1) {
-      sections[currentSectionIndex + 1].scrollIntoView({ behavior: "smooth" });
-      currentSectionIndex++;
+    sections[currentSectionIndex + 1].scrollIntoView({ behavior: 'smooth' });
+    currentSectionIndex++;
+  }
 
-      if (currentSectionIndex === sections.length - 1) {
-        scrollButton.classList.add('no-display');
-        console.log(scrollButton);
-    }
+  if (currentSectionIndex === sections.length - 1) {
+    downScrollButton.classList.add('no-display');
+  }
+}
+
+document.addEventListener('wheel', (event) => {
+  if (event.deltaY > 0) {
+    handleScrollDown();
+    console.log('Scrolled down');
+  } else if (event.deltaY < 0) {
+    // handleScrollUp();
+    console.log('Scrolled up');
   }
 });
+
+document.addEventListener('keydown', (event) => {
+  if (event.key === ' ' || event.key === 'ArrowDown') {
+    event.preventDefault();
+    console.log(event.key)
+    handleScrollDown();
+  }
+
+  if (event.key === 'ArrowUp') {
+    // handleScrollUp();
+  }
+});
+
+downScrollButton.addEventListener("click", handleScrollDown);
